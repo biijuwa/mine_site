65d123049673c489cbc9c0e25794aef33a07e396
Bijaya Limbu <bijaya.l@shikhartech.com>
2024-06-10 15:12:09 +0545

    test

---
+++ hover.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const bijayaText = document.querySelector('.hover-change');
+  
+    bijayaText.addEventListener('mouseover', function() {
+      bijayaText.textContent = 'I\'m a Technical Writer.';
+    });
+  
+    bijayaText.addEventListener('mouseout', function() {
+      bijayaText.textContent = 'I\'m Bijaya';
+    });
+  });
+  
